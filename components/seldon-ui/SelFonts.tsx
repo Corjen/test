97c8464f99e332a33b6b504cba19a3a6d969846e
corjen <corjen@reverseddigital.com>
2024-12-02 17:04:39 +0100

    Update from Seldon

--- components/seldon-ui/SelFonts.tsx
+++ components/seldon-ui/SelFonts.tsx
@@ -2,8 +2,18 @@ export function SelFonts() {
   return (
     <>
       <link
-        key="Inter"
-        href="https://fonts.googleapis.com/css2?family=Inter%3Awght%40100%3B200%3B300%3B400%3B500%3B600%3B700%3B800%3B900&display=swap"
+        key="Roboto"
+        href="https://fonts.googleapis.com/css2?family=Roboto%3Awght%40100%3B200%3B300%3B400%3B500%3B600%3B700%3B800%3B900&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        key="Libre Baskerville"
+        href="https://fonts.googleapis.com/css2?family=Libre+Baskerville%3Awght%40100%3B200%3B300%3B400%3B500%3B600%3B700%3B800%3B900&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        key="Playfair Display"
+        href="https://fonts.googleapis.com/css2?family=Playfair+Display%3Awght%40100%3B200%3B300%3B400%3B500%3B600%3B700%3B800%3B900&display=swap"
         rel="stylesheet"
       />
     </>
